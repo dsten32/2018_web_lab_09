850db2e4e4b5acc9b56035ec0fa23fbe2412f957
Cameron Grout <cameron.grout@waikato.ac.nz>
2018-12-02 21:31:40 +1300

    2018S

---
+++ src/ictgradschool/web/lab09/ex01/ProfileServlet.java
@@ -0,0 +1,22 @@
+package ictgradschool.web.lab09.ex01;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // request.getParameter accesses parameters from the submitted form
+        String fname = request.getParameter("fname");
+
+        // printWriter is used to write to the HTML document
+        PrintWriter out = response.getWriter();
+
+        out.println("First Name: " + fname);
+
+    }
+}
