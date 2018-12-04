488d7aa837c1070b98dcd04dc90233459a817dff
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-12-05 12:21:18 +1300

    did ex03-ex04

--- src/ictgradschool/web/lab09/ex02/ArticleServlet.java
+++ src/ictgradschool/web/lab09/ex02/ArticleServlet.java
@@ -6,14 +6,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ArticleServlet")
 public class ArticleServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String aTitle = request.getParameter("aTitle");
+        String aAuthor = request.getParameter("aAuthor");
+        String aContent = request.getParameter("aContent");
+        String aGenre = request.getParameter("aType");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>"+aTitle+" using POST</h1>");
+
+        out.println("<p> by: "+aAuthor+"</p>");
+        out.println("<p> Genre: "+aGenre+"</p>");
+        out.println("<p> "+aContent+"</p>");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String aTitle = request.getParameter("aTitle");
+        String aAuthor = request.getParameter("aAuthor");
+        String aContent = request.getParameter("aContent");
+        String aGenre = request.getParameter("aType");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>"+aTitle+"</h1>");
+
+        out.println("<p> by: "+aAuthor+"</p>");
+        out.println("<p> Genre: "+aGenre+"</p>");
+        out.println("<p> "+aContent+"</p>");
+
 
     }
 }
